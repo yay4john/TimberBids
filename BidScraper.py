2b5d219e32889c58451c668913ebcde80bbd00d6
John Cox <yay4john@users.noreply.github.com>
2021-11-03 16:59:49 -0700

    Updates to scraper

--- BidScraper.py
+++ BidScraper.py
@@ -1,11 +1,13 @@
 # Import the required packages
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+import requests                     # get html, submit posts
+from bs4 import BeautifulSoup       # parse and search through html
+import pandas as pd                 # dataframes!
+import json                         # convert string to dictionary
 
 # Set address to the ODF timber bid results page and download
+baseURL = 'https://apps.odf.oregon.gov/Divisions/management/asset_management/'
 link = 'https://apps.odf.oregon.gov/Divisions/management/asset_management/ResultsSelection.asp?optDist=Astoria&Arc=Current'
-page = requests.get(link).text
+page = requests.get(link, timeout=10.0).text
 
 # Parse
 soup = BeautifulSoup(page, 'html.parser')
@@ -14,20 +16,37 @@ soup = BeautifulSoup(page, 'html.parser')
 tables = soup.find_all('table',attrs={'width':'710'})
 
 # Create empty dataframe to hold details
-df = pd.DataFrame(columns=['SaleName','SaleID','BidOpening','Link'])
+df = pd.DataFrame(columns=['SaleName','SaleID','BidOpeningDate','Link','PostInput'])
 
 # for each table in the list, pull out each row and its columns
 for tb in tables:
     for row in tb.find_all('tr'):
         columns = row.find_all('td')
 
-        # if the column is not an empty list, grab the 4 fields and append them to the df
+        postString = ''
+
+        # if the column is not an empty list, grab the 4 main fields, plus the details needed for post request
         if(columns != []):
             SaleName = columns[0].text.strip()
             SaleID = columns[1].text.strip()
-            BidOpening = columns[2].text.strip()
-            Link = columns[3].form['action']
+            BidOpeningDate = columns[2].text.strip()
+            Link = baseURL + columns[3].form['action']
+            # there are 9 fields used in the post request, so build the string that contains them all
+            for i in range(9):
+                postString = postString + '"' + columns[3].find_all('input')[i]['name'] + '":"' + columns[3].find_all('input')[i]['value'] + '",'
+            PostInput = '{' + postString[:-1] + '}'
 
-            df = df.append({'SaleName': SaleName,  'SaleID': SaleID, 'BidOpening': BidOpening, 'Link': Link}, ignore_index=True)
+            df = df.append({'SaleName': SaleName,  'SaleID': SaleID, 'BidOpeningDate': BidOpeningDate, 'Link': Link, 'PostInput': PostInput}, ignore_index=True)
 
-print(df)
+# Clean spaces and appostrophes out of the link
+df['Link'] = df['Link'].str.replace(' ','%20')
+df['Link'] = df['Link'].str.replace("'",'%27')
+
+
+# for now, just work with one sale. Will need to use that json bit in the future loop, it converts the string into a dictionary
+newURL = df.iloc[0,3]
+poststuff = json.loads(df.iloc[0,4])
+
+x = requests.post(newURL,data = poststuff,timeout=10.0)
+print(x.text)
+
