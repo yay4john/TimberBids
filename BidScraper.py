e93f32b925e527bb460d533784441ff85eabc6f5
John Cox <yay4john@users.noreply.github.com>
2021-11-03 11:55:28 -0700

    Revert to requests

--- BidScraper.py
+++ BidScraper.py
@@ -1,53 +1,33 @@
 # Import the required packages
-from playwright.sync_api import sync_playwright
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#
-# This function will download the content of the address of the link using the package Playwright.
-#
-def get_webpage_content(url):
-    with sync_playwright() as p:
-        browser = p.chromium.launch()
-        page = browser.new_page()
-        page.goto(url)
-        page.wait_for_load_state('networkidle')
-        result = page.content()
-        browser.close()
-    return result
-
-#
-# This function gets the table body and return a list of rows with data fields.
-#
-def get_table_body_as_lists(table_obj):
-    result = []
-    table_body = table_obj.find('tbody')
-    table_rows = table_body.find_all('tr')
-    for row in table_rows:
-        curr_row = []
-        row_fields = row.find_all('td')
-        for field_obj in row_fields:
-            curr_row.append(field_obj.getText().strip())
-        result.append(curr_row)
-    return result
-
 # Set address to the ODF timber bid results page and download
 link = 'https://apps.odf.oregon.gov/Divisions/management/asset_management/ResultsSelection.asp?optDist=Astoria&Arc=Current'
-content = get_webpage_content(link)
+page = requests.get(link).text
 
 # Parse
-soup = BeautifulSoup(content, 'html.parser')
+soup = BeautifulSoup(page, 'html.parser')
 
-# Find the first table in the web page
-table = soup.find('table')
+# Find the subtables that contain auction details and store them in a list
+tables = soup.find_all('table',attrs={'width':'710'})
 
-# Get data from the table head and table body
-table_body = get_table_body_as_lists(table)
+# Create empty dataframe to hold details
+df = pd.DataFrame(columns=['SaleName','SaleID','BidOpening','Link'])
 
-# Remove the first two sub-lists
-del table_body[0:2]
+# for each table in the list, pull out each row and its columns
+for tb in tables:
+    for row in tb.find_all('tr'):
+        columns = row.find_all('td')
 
-# Convert to a data frame
-df = pd.DataFrame(table_body)
+        # if the column is not an empty list, grab the 4 fields and append them to the df
+        if(columns != []):
+            SaleName = columns[0].text.strip()
+            SaleID = columns[1].text.strip()
+            BidOpening = columns[2].text.strip()
+            Link = columns[3].form['action']
+
+            df = df.append({'SaleName': SaleName,  'SaleID': SaleID, 'BidOpening': BidOpening, 'Link': Link}, ignore_index=True)
 
 print(df)
