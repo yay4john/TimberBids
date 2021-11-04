b98722f40ad7834b585edc78618cf7f86c466f65
John Cox <yay4john@users.noreply.github.com>
2021-11-03 20:35:47 -0700

    Added appraisals

--- BidScraper.py
+++ BidScraper.py
@@ -42,11 +42,49 @@ for tb in tables:
 df['Link'] = df['Link'].str.replace(' ','%20')
 df['Link'] = df['Link'].str.replace("'",'%27')
 
-
 # for now, just work with one sale. Will need to use that json bit in the future loop, it converts the string into a dictionary
 newURL = df.iloc[0,3]
 poststuff = json.loads(df.iloc[0,4])
 
-x = requests.post(newURL,data = poststuff,timeout=10.0)
-print(x.text)
+auction = requests.post(newURL,data = poststuff,timeout=10.0).text
+auctionSoup = BeautifulSoup(auction, 'html.parser')
 
+appraisal = auctionSoup.select('body > p:nth-child(4) > table')
+
+# Create empty dataframe to hold details
+dfAppraisal = pd.DataFrame(columns=['Volume','Species','Price'])
+
+# for each table in the list, pull out each row and its columns
+for tb in appraisal:
+    for row in tb.find_all('tr'):
+        columns = row.find_all('td')
+
+        # if the column is not an empty list, grab the 4 main fields, plus the details needed for post request
+        if(columns != []):
+            Volume = columns[0].text.strip()
+            Species = columns[1].text.strip()
+            Price = columns[2].text.strip()
+
+            dfAppraisal = dfAppraisal.append({'Volume': Volume,  'Species': Species, 'Price': Price}, ignore_index=True)
+
+print(dfAppraisal)
+
+winners = auctionSoup.select('body > table:nth-child(7)')
+print(winners)
+# Create empty dataframe to hold details
+dfBids = pd.DataFrame(columns=['Bidder','Species','Price'])
+
+# for each table in the list, pull out each row and its columns
+for tb in winners:
+    for row in tb.find_all('tr'):
+        columns = row.find_all('td')
+
+        # if the column is not an empty list, grab the 4 main fields, plus the details needed for post request
+        if(columns != []):
+            Bidder = columns[0].text.strip()
+            Species = columns[2].text.strip()
+            Price = columns[1].text.strip()
+
+            dfBids = dfBids.append({'Bidder': Bidder,  'Species': Species, 'Price': Price}, ignore_index=True)
+
+print(dfBids)
